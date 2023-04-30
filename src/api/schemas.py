6e90cea96e6f55db65d1a4c6ff0ab645ea444a62
Tereshchenko Vlad <ddiavall@yandex.ru>
2023-05-01 01:31:37 +0300

    [fix]: modified Paragraph class

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,24 +1,45 @@
+from typing import Any
 from pydantic import BaseModel
 
 class Paragraph(BaseModel):
-    page_breake_before: str
-    keep_lines_together: str
-    keep_with_next: str
-    outline_level: str
-    text: str
-    count_of_sp_sbl: str
-    count_sbl: str
-    lowercase: str
-    uppercase: str
-    last_sbl: str
-    first_key: str
-    bold: str
-    italics: str
+    page_breake_before: Any | None = None
+    keep_lines_together: Any | None = None
+    keep_with_next: Any | None = None
+    outline_level: Any | None = None
+    text: str | None = None
+    count_of_sp_sbl: float | None = None
+    count_sbl: float | None = None
+    lowercase: bool | None = None
+    uppercase: bool | None = None
+    last_sbl: str | None = None
+    first_key: str | None = None
+    bold: Any | None = None
+    italics: Any | None = None
+    indent: float | None = None
+    line_spacing: float | None = None
+    alignment: Any | None = None
+    mrgrg: Any | None = None
+    mrglf: Any | None = None
+    mrgtop: Any | None = None
+    mrgbtm: Any | None = None
+    font_name: str | None = None
+    text_size: float | None = None
+    underlining: Any | None = None
+    sub_text: Any | None = None
+    super_text: Any | None = None
+    color_text: str | None = None
+    no_change_fontname: bool | None = None
+    no_change_text_size: bool | None = None
+    current_element_mark: str | None = None
+    class Config:
+        orm_model = True
 
 class Document(BaseModel):
     owner: str
     time: str
     content: dict[str, Paragraph]
+    class Config:
+        orm_model = True
 
 class Gost_params(BaseModel):
     id: int
